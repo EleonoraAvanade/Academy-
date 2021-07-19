624925cbbdcd10c79ca24100b38dadc67a7fb717
Antonia Sacchitella <Antonia.Sacchitella@icubed.it>
2021-07-19 16:51:26 +0200

    esercitazione numeri complessi

--- Week2/Week2.Demo/Week2.ComplexNumber/NumeroComplesso.cs
+++ Week2/Week2.Demo/Week2.ComplexNumber/NumeroComplesso.cs
@@ -2,7 +2,107 @@
 
 namespace Week2.ComplexNumber
 {
+    public enum Format
+    {
+        Algebrico,
+        Parentesi
+    }
+
     public class NumeroComplesso
     {
+
+        public double Reale { get; set; }
+        public double Immaginaria { get; set; }
+
+        public double Modulo
+        {
+            get
+            {
+                return Math.Sqrt(Math.Pow(Reale, 2) + Math.Pow(Immaginaria, 2));
+            }
+        }
+
+        public NumeroComplesso Coniugato
+        {
+            get
+            {
+                return new NumeroComplesso()
+                {
+                    Reale = this.Reale,
+                    Immaginaria = -this.Immaginaria
+                };
+            }
+        }
+
+        public NumeroComplesso Somma(NumeroComplesso value)
+        {
+            //NumeroComplesso risultato = new NumeroComplesso();
+            //risultato.Reale = this.Reale + value.Reale;
+            //risultato.Immaginaria = this.Immaginaria + value.Immaginaria;
+            //return risultato;
+
+            return new NumeroComplesso
+            {
+                Reale = this.Reale + value.Reale,
+                Immaginaria = this.Immaginaria + value.Immaginaria
+            };
+        }
+
+        public NumeroComplesso Differenza(NumeroComplesso value)
+        {
+            double parteReale = value.Reale > 0 ? this.Reale - value.Reale : this.Reale + value.Reale;
+            double parteImmaginaria = value.Immaginaria > 0 ? this.Immaginaria - value.Immaginaria : this.Immaginaria + value.Immaginaria;
+            return new NumeroComplesso
+            {
+                Reale = parteReale,
+                Immaginaria = parteImmaginaria
+            };
+        }
+
+        public NumeroComplesso Moltiplicazione(NumeroComplesso value)
+        {
+            return new NumeroComplesso
+            {
+                Reale = this.Reale * value.Reale - this.Immaginaria * value.Immaginaria,
+                Immaginaria = this.Reale * value.Immaginaria + this.Immaginaria * value.Reale
+            };
+        }
+
+        public NumeroComplesso Divisione(NumeroComplesso value)
+        {
+            double denom = Math.Pow(value.Reale, 2) + Math.Pow(value.Immaginaria, 2);
+            if (denom != 0)
+            {
+                throw new DivideByZeroException();
+            }
+            double realeRisul = (this.Reale * value.Reale + this.Immaginaria * value.Immaginaria) / denom;
+            double immRisul = (this.Immaginaria * value.Reale - this.Reale * value.Immaginaria) / denom;
+            return new NumeroComplesso
+            {
+                Reale = realeRisul,
+                Immaginaria = immRisul
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"Reale: {Reale} - Immaginaria: {Immaginaria}";
+        }
+
+        public string ToString(Format format)
+        {
+            switch (format)
+            {
+                case Format.Algebrico:
+                    return $"{Reale} { (Immaginaria < 0 ? "-" : "+") } i {Math.Abs(Immaginaria)}";
+                    break;
+                case Format.Parentesi:
+                    return $"({Reale}, {Immaginaria})";
+                    break;
+                default:
+                    return this.ToString();
+            }
+        }
+
     }
 }
--- Week2/Week2.Demo/Week2.EsercitazioneComplex/Program.cs
+++ Week2/Week2.Demo/Week2.EsercitazioneComplex/Program.cs
@@ -7,7 +7,49 @@ namespace Week2.EsercitazioneComplex
     {
         static void Main(string[] args)
         {
-            NumeroComplesso nc = new NumeroComplesso();
+            Console.WriteLine("Inserisci parte reale del primo numero complesso");
+            double.TryParse(Console.ReadLine(), out double reale);
+            Console.WriteLine("Inserisci parte immaginaria del primo numero complesso");
+            double.TryParse(Console.ReadLine(), out double immaginaria);
+            NumeroComplesso nc = new NumeroComplesso
+            {
+                Reale = reale,
+                Immaginaria = immaginaria
+            };
+            Console.WriteLine("Secondo numero - Parte reale");
+            double.TryParse(Console.ReadLine(), out reale);
+            Console.WriteLine("Secondo numero - Parte immaginaria");
+            double.TryParse(Console.ReadLine(), out immaginaria);
+            NumeroComplesso nc2 = new NumeroComplesso()
+            {
+                Reale = reale,
+                Immaginaria = immaginaria
+            };
+
+            Console.WriteLine("Numeri inseriti \n {0} \n {1}", nc, nc2);
+            Console.WriteLine("Numeri inseriti \n {0}\n {1}", nc.ToString(Format.Algebrico), nc2.ToString(Format.Algebrico));
+
+            Console.WriteLine(@"Scegli l'operazione + - / *");
+            var op = Console.Read();
+            NumeroComplesso risultato = new NumeroComplesso();
+            switch (op)
+            {
+                case '+':
+                    risultato = nc.Somma(nc2);
+                    break;
+                case '-':
+                    risultato = nc.Differenza(nc2);
+                    break;
+                case '*':
+                    risultato = nc.Moltiplicazione(nc2);
+                    break;
+                case '/':
+                    risultato = nc.Divisione(nc2);
+                    break;
+            }
+            Console.WriteLine("Risultato: {0}", risultato.ToString(Format.Parentesi));
+            Console.WriteLine("Risultato: {0}", risultato.ToString(Format.Algebrico));
+
         }
     }
 }
